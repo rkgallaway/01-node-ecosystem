b9f15cb4bb828a96277638462b67ed5fba4d43b6
rkgallaway <rkgallaway@msn.com>
2018-12-18 18:17:51 -0800

    tests passing on add and subtract

--- __tests__/arithmetic.test.js
+++ __tests__/arithmetic.test.js
@@ -2,3 +2,18 @@
 
 const arithmetic = require('../lib/arithmetic.js');
 
+describe('adder module', () => {
+  it ('can add 2 numbers', () => {
+    let expected = 2;
+    let sum = arithmetic.add(1, 1);
+    expect( sum).toEqual(expected);
+  });
+});
+
+describe('subtracter module', () => {
+  it('can subtract 2 numbers', () => {
+    let expected = 0;
+    let difference = arithmetic.subtract(1, 1);
+    expect(difference).toEqual(expected);
+  });
+});
